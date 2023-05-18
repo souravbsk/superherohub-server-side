03bc5c7b519db0ec5f0e415896c75bee379a5aa5
souravbsk <souravbsk01@gmail.com>
2023-05-18 20:57:38 +0600

    data user name pass setup done

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// middleware 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
-
-
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://<username>:<password>@cluster0.pr3rbd0.mongodb.net/?retryWrites=true&w=majority`;
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.pr3rbd0.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,7 +25,9 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -38,19 +35,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Super Hero server running");
+});
 
-
-
-
-
-
-
-
-
-app.get("/",(req,res) => {
-    res.send("Super Hero server running")
-})
-
-app.listen(port,() => {
-    console.log(`super hero running on this port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`super hero running on this port ${port}`);
+});
